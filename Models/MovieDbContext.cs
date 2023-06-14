d1e9e6f99fb76af2d842933d3c513ab7bd2b99a7
dildeep15 <dildeep15@gmail.com>
2023-06-14 10:03:47 +0200

    Created ConnectionString & MovieDbContext class

---
+++ Models/MovieDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace MovieCharacterAPI.Models
+{
+    public class MovieDbContext : DbContext
+    {
+        public MovieDbContext([NotNullAttribute] DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -2,11 +2,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using MovieCharacterAPI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +28,8 @@ namespace MovieCharacterAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Configure ConnectionString
+            services.AddDbContext<MovieDbContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
